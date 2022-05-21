483f6043496753e28bb68e33bcf4704673412816
Edmilson <edmilsonribeiro.17072005@gmail.com>
2022-05-20 22:40:08 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int dividendo, divisor, resto;
+
+Console.WriteLine("maxímo divisor comum (metodo interativo)\n");
+
+Console.Write("digite o 1º numero (a): ");
+dividendo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite o 2º numero (b): ");
+divisor = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    resto = dividendo % divisor;
+    dividendo = divisor;
+    divisor = resto;
+}
+ while (resto != 0);
+
+ Console.WriteLine($"MDC(a , b) = {dividendo}");
